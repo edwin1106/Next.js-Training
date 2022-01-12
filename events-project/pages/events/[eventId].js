0d34cf17dabc638cbeb9bc2adbe9b042ab435c74
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2022-01-12 18:33:33 -0500

    Render event detail

--- events-project/pages/events/[eventId].js
+++ events-project/pages/events/[eventId].js
@@ -1,12 +1,37 @@
-const EventDetailPage = () => {
-    return(
-        <div>
-            <h1>
-                Event Detail
-            </h1>
-        </div>
-    )
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventContent from "../../components/event-detail/event-content";
 
-}
+const EventDetailPage = () => {
+  const router = useRouter();
+
+  const { eventId } = router.query;
+
+  console.log(eventId, "router");
+  const event = getEventById(eventId);
+
+  console.log(event, "event");
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+};
 
 export default EventDetailPage;
