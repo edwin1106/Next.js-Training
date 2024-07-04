1ec05afe0c20b73c7a5120e7a2a8e9b990dce6c5
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-03 22:33:02 -0500

    Adding header component to main layout

--- foodies-app/src/app/page.tsx
+++ foodies-app/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>Home Page</h1>
+    </main>
+  );
 }
