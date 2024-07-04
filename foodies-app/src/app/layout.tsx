ca3fae9f4ec2f03ab06d0721752bc42fe4cb5202
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-04 18:57:00 -0500

    Creating a main page for project

--- foodies-app/src/app/layout.tsx
+++ foodies-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@styles/globals.css';
-import MainHeader from '@/components/mainHeader';
+import MainHeader from '@/components/mainHeader/mainHeader';
 
 const inter = Inter({ subsets: ['latin'] });
 
--- foodies-app/src/app/page.tsx
+++ foodies-app/src/app/page.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import ImageSlideshow from '@/components/imageSlideshow/imageSlideshow';
+
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>Home Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+            pariatur voluptatibus. Sed, tenetur repellat praesentium labore
+            facere ea eveniet? Itaque, aut. Itaque amet maxime fuga officia
+            nihil numquam voluptas quam?
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
