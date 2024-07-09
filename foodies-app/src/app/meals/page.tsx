e71965db63d89f55f7996efd4af9e972144c526b
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-08 20:09:15 -0500

    Fetching data from supabase database

--- foodies-app/src/app/meals/page.tsx
+++ foodies-app/src/app/meals/page.tsx
@@ -1,4 +1,12 @@
-const MealsPage = () => {
-  return <h1>Meals Page</h1>;
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
+
+const MealsPage = async () => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  let { data: meals, error } = await supabase.from('meals').select();
+
+  return <pre>{JSON.stringify(meals, null, 2)}</pre>;
 };
 export default MealsPage;
