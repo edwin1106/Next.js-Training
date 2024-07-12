57ca154488bd26341da4ad7413f0d25e7d69207c
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-11 22:42:46 -0500

    Adding general not found page

---
+++ foodies-app/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, we could not find the request page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
