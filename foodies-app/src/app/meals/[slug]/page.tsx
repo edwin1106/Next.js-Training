3015776dd69fb885e8498c7e4f9f87fa02327f8d
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-13 18:57:56 -0500

    Developing meals slug page

--- foodies-app/src/app/meals/[slug]/page.tsx
+++ foodies-app/src/app/meals/[slug]/page.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+import getMealBySlug from '@/services/getMealBySlug';
+
 interface SlugMealPageProps {
   params: { slug: string };
 }
-const SlugMealPage = ({ params }: SlugMealPageProps) => {
-  return <h1>{params.slug} Meal</h1>;
+const SlugMealPage = async ({ params }: SlugMealPageProps) => {
+  const meal = await getMealBySlug({ slug: 'spicy-curry' });
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1></h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${'email'}`}></a>
+          </p>
+          <p className={classes.summary}></p>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
 };
 export default SlugMealPage;
