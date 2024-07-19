08a9275a57ce274aa7325e5a645d3f49b88cf211
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-18 20:50:20 -0500

    Added an image picker preview

--- foodies-app/src/components/meals/imagePicker.tsx
+++ foodies-app/src/components/meals/imagePicker.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import React, { InputHTMLAttributes, useState } from 'react';
 import classes from './imagePicker.module.css';
-import React from 'react';
+import Image from 'next/image';
 
 interface ImagePickerProps {
   label?: string;
@@ -9,16 +10,47 @@ interface ImagePickerProps {
 }
 
 const ImagePicker = ({ label, name }: ImagePickerProps) => {
+  const [pickedImage, setPickedImage] = useState<string | ArrayBuffer | null>(
+    null
+  );
   const imageInput = React.useRef<HTMLInputElement>(null);
 
   const handlePickClick = () => {
     if (imageInput.current) imageInput.current.click();
+  };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files;
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader?.readAsDataURL(file[0]);
   };
   return (
     <>
       <div className={classes.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={classes.controls}>
+          <div className={classes.preview}>
+            {!pickedImage ? (
+              <p>No image picked yet.</p>
+            ) : (
+              <Image
+                src={pickedImage as string}
+                alt="The image select by the user."
+                fill
+              />
+            )}
+          </div>
           <input
             className={classes.input}
             type="file"
@@ -26,6 +58,7 @@ const ImagePicker = ({ label, name }: ImagePickerProps) => {
             accept="image/png, image/jpeg"
             name={name}
             ref={imageInput}
+            onChange={handleImageChange}
           />
         </div>
         <button
