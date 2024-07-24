d75526786e587ab0fd6296221e449de21a0c6c5f
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-23 20:16:18 -0500

    Adding specifig error page to share meals

---
+++ foodies-app/src/app/meals/share/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const ErrorPage = () => {
+  return (
+    <main className="error">
+      <h1>An error accured!</h1>
+      <p>Failed to create meal.</p>
+    </main>
+  );
+};
+export default ErrorPage;
