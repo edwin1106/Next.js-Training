305cfbc08f3631ae92e96544457f4321ae747550
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-07-27 19:49:54 -0500

    Changed news page content

--- render-deep-dive-project/src/app/news/page.tsx
+++ render-deep-dive-project/src/app/news/page.tsx
@@ -1,4 +1,21 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/first-news">First News Item</Link>
+        </li>
+        <li>
+          <Link href="/news/second-news">Second News Item</Link>
+        </li>
+        <li>
+          <Link href="/news/third-news">Third News Item</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 export default NewsPage;
