6a2fe52344173ff1eca454e8643c2b272baf5482
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-08-12 19:29:30 -0500

    Styling news slug page

--- render-deep-dive-project/src/app/news/[slug]/page.tsx
+++ render-deep-dive-project/src/app/news/[slug]/page.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image';
+import { DUMMY_NEWS } from '../../../../dummy-news';
+import { notFound } from 'next/navigation';
+
 interface NewsDetailProps {
   params: { slug: string };
 }
 const NewsDetailPage = ({ params }: NewsDetailProps) => {
   const { slug } = params;
-  return <h1>{`This is the new with Id: ${slug}`}</h1>;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
+  if (!newsItem) notFound();
+  return (
+    <article className="news-article">
+      <header>
+        <Image
+          alt={newsItem.title}
+          src={`/images/news/${newsItem?.image}`}
+          width={100}
+          height={110}
+        ></Image>
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  );
 };
 export default NewsDetailPage;
