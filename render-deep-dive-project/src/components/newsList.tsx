5a61b5fbf014efa980837e92c0ea1c7f1eec6e52
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-08-13 23:00:37 -0500

    Adding a componenet to render a list of news

---
+++ render-deep-dive-project/src/components/newsList.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+type New = {
+  id: string;
+  slug: string;
+  title: string;
+  image: string;
+  date: string;
+  content: string;
+};
+
+interface NewsListProps {
+  news: New[];
+}
+
+const NewsList = ({ news }: NewsListProps) => {
+  return (
+    <ul className="news-list">
+      {news.map((newsItem) => (
+        <li key={newsItem.id}>
+          <Link href={`/news/${newsItem.slug}`}>
+            <Image
+              alt={newsItem.title}
+              src={`/images/news/${newsItem.image}`}
+              width={300}
+              height={300}
+            ></Image>
+            <span>{newsItem.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default NewsList;
