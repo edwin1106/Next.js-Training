2c4ec5dc711924b8748681e31298192de5e70779
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-08-13 23:00:59 -0500

    Refactoring News Page

--- render-deep-dive-project/src/app/news/page.tsx
+++ render-deep-dive-project/src/app/news/page.tsx
@@ -1,26 +1,11 @@
-import Link from 'next/link';
-import { DUMMY_NEWS } from '../../../dummy-news';
-import Image from 'next/image';
+import { DUMMY_NEWS } from '../../lib/dummy-news';
+import NewsList from '@/components/newsList';
 
 const NewsPage = () => {
   return (
     <>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newsItem) => (
-          <li key={newsItem.id}>
-            <Link href={`/news/${newsItem.slug}`}>
-              <Image
-                alt={newsItem.title}
-                src={`/images/news/${newsItem.image}`}
-                width={300}
-                height={300}
-              ></Image>
-              <span>{newsItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 };
