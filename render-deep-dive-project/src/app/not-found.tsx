f03f87e9f7f6e04712048869b2b7086b097cd372
edwinosoriogaviria <30908546+edwin1106@users.noreply.github.com>
2024-09-04 21:35:18 -0500

    Moving pages

--- render-deep-dive-project/src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = () => {
-  return (
-    <div id="error">
-      <h1>Not Found!</h1>
-      <p>Unfortunately, we could not find the requested article.</p>
-    </div>
-  );
-};
-export default NotFoundPage;
